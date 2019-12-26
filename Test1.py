fc08078454a8ca6b6c556a1bd5f49a54039e80e5
Ali S <mas19870@gmail.com>
2019-12-26 12:48:39 +0100

    Initial commit

---
+++ Test1.py
@@ -0,0 +1,21 @@
+class BookShelf:
+    def __init__(self, *books):
+        self.books = books
+
+
+    def __str__(self):
+        return f"<Book shelf contains {len(self.books)} Books>"
+
+class Book:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"<Book {self.name}>"
+
+book1 = Book('Harry Potter')
+book2 = Book ("Letters from Khalil Gibran")
+book3 = Book ("Python 101")
+
+shelf = BookShelf(book1,book2 ,book3)
+print(shelf)
